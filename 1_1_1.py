a07c1e4fff31fcfeee0262bafc8b34cf5a63dc86
Aayush Sanghavi <aayushsanghavi26@gmail.com>
2016-08-15 00:47:42 +0530

    Completed the get_values function

--- 1_1_1.py
+++ 1_1_1.py
@@ -8,11 +8,13 @@ page_url = "http://www.healthcaremagic.com/community"
 
 categories = []
 
-#this function retrives the title, url and number of each category
+#this function retrives the title, url and number of each category and number of questions asked
 def get_values(page_url):
 	page = urllib2.urlopen(page_url)
 	soup = BeautifulSoup(page,'lxml')
 	page.close()
+	
+	#this segment of code retrives title, url and number of each category
 	categories_a = soup.find_all("a",class_="questionTitle")
 	for category_a in categories_a:
 		title = category_a.string
@@ -27,4 +29,17 @@ def get_values(page_url):
 		d = [title,url,number]
 		categories.append(d)
 
+	#this segment of code retrives the number of questions asked in the category
+	i = 0
+	categories_span = soup.find_all("span",style="display: block;font-size:10px; color:#999;")
+	for category_span in categories_span:
+		questions = category_span.string
+		match = re.search(r'\d+',questions)
+		match = match.group()
+		number = match
+		questions = str(number)+' questions'
+		categories[i].append(questions)
+		i += 1
+
+#this runs the function
 get_values(page_url)
