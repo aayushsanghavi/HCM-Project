61a8700b1b76b6f4b6c435ba118bdf6632382990
Aayush Sanghavi <aayushsanghavi26@gmail.com>
2016-08-17 09:51:18 +0530

    Completed both function and extraction working

--- 1_1_2.py
+++ 1_1_2.py
@@ -5,14 +5,17 @@ from bs4 import BeautifulSoup
 
 #url of the desired webpage
 page_url = "http://www.healthcaremagic.com/topics/disease-and-conditions"
-page = urllib2.urlopen(page_url)
-soup = BeautifulSoup(page,'lxml')
-page.close()
 categories = []
+continue_extraction = True
+i = 0
 
 #this segment of code retrives the url of the next page
-def get_next_page(page_url):
+def get_next_page(page_link):
+	page = urllib2.urlopen(page_link)
+	soup = BeautifulSoup(page,'lxml')
+	get_values(soup)
 	page_li = soup.find_all("a",class_="box");
+	page.close()
 	for li in page_li:
 		page_li_value = li.string
 		page_li_url = li.get('href')
@@ -21,20 +24,21 @@ def get_next_page(page_url):
 		match = re.search(r'\D+',page_li_value)
 		if match:
 			match = match.group()
-			next = match
-			if next:
-				next_page_url = "www.healthcaremagic.com" + page_li_url
-				return next_page_url
-
+			if match:
+				next_page_url = "http://www.healthcaremagic.com" + page_li_url
+		else:
+			next_page_url = None
+	return next_page_url
+		
 #this function retrives the title, url and number of each category for the specific url
-def get_values(page_url):
+def get_values(soup):
 	ul = soup.find("ul",class_="itemListUL anchorListing")
 	all_li = ul.find_all("li")
 	for li in all_li:
 		title = li.a.string
 		title = title.lstrip()
 		title = title.rstrip()
-		url = "http://www.healthcaremagic.com"+li.a.get('href')
+		url = "http://www.healthcaremagic.com" + li.a.get('href')
 		url = url.lstrip()
 		url = url.rstrip()
 		match = re.search(r'\d+',url)
@@ -43,5 +47,11 @@ def get_values(page_url):
 		d = [title,url,number]
 		categories.append(d)
 
-#next_page_url = get_next_page(page_url)
-#get_values(page_url)
+#this loop extracts all the required information from all the pages
+while continue_extraction:
+	next_page = get_next_page(page_url)
+	if next_page == None:
+		continue_extraction = False
+	else:
+		pass
+	page_url = next_page
