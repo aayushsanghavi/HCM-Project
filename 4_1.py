2c215b934964e9897fcc4a4d65d9aec32a3ce921
Aayush Sanghavi <aayushsanghavi26@gmail.com>
2016-08-28 20:22:17 +0530

    Finished level 1 extraction of doctors page

---
+++ 4_1.py
@@ -0,0 +1,85 @@
+#importing python modules and libraries
+import urllib2
+import re
+import csv
+from bs4 import BeautifulSoup
+
+#url of the desired webpage
+page_url = "http://www.healthcaremagic.com/doctors"
+
+#this code creates a file categories.csv and stores all the information retrived
+file = open('doctors.csv','wb')
+write = csv.writer(file,delimiter=" ")
+
+#this function retrives the next page url and the data from that page
+def get_next_page(page_link):
+	page = urllib2.urlopen(page_link)
+	soup = BeautifulSoup(page,'lxml')
+	div = soup.find("div",id="dataDiv")
+	page.close()
+	get_values(div)
+	
+	#this segment of code retrives the url of the next page
+	pagination = div.find("div",id="paginationDiv")
+	page_li = pagination.find_all("a",class_="box")
+	for li in page_li:
+		page_li_value = li.string
+		page_li_url = li.get('href')
+		page_li_value = page_li_value.lstrip()
+		page_li_value = page_li_value.rstrip()
+		match = re.search(r'\D+',page_li_value)
+		if match:
+			match = match.group()
+			if match:
+				next_page_url = "http://www.healthcaremagic.com" + page_li_url
+		else:
+			next_page_url = None
+	return next_page_url
+		
+#this function retrives the title, url, id, specialisation, number of reviews and location of each doctor
+def get_values(content):
+	all_div = content.find_all("div",id="singleDiv")
+	for div in all_div:
+		inner_div = div.find("div",style="float:left;width:100%;padding-bottom:5px;")
+		spans = div.find_all("span",style="font-size:11px;")
+		review_div = div.find("div",style="font-size:11px;width:90%;")
+
+		title = inner_div.a.string
+		title = title.lstrip()
+		title = title.rstrip()
+		
+		url = "http://www.healthcaremagic.com" + inner_div.a.get('href')
+		url = url.lstrip()
+		url = url.rstrip()
+		
+		match = re.search(r'\d+',url)
+		match = match.group()
+		number = match
+		
+		specialisation = inner_div.span.string
+		specialisation = specialisation.lstrip(",  ")
+		specialisation = specialisation.rstrip()
+		
+		location = ""
+		for span in spans:
+			location += span.string
+		location = location.lstrip()
+		location = location.rstrip()
+
+		reviews = "0"
+		if review_div:
+			reviews = review_div.text
+			reviews = reviews.lstrip()
+			reviews = reviews.rstrip()
+
+		d = [title,specialisation,url,number,location,reviews]
+		write.writerow(d)
+
+#this loop extracts all the required information from all the pages
+continue_extraction = True
+while continue_extraction:
+	next_page = get_next_page(page_url)
+	if next_page == None:
+		continue_extraction = False
+	else:
+		page_url = next_page
