4b50ed467b841d7d65288e06eecefe8b42d72e71
Aayush Sanghavi <aayushsanghavi26@gmail.com>
2016-09-23 12:39:52 +0530

    Some minor chnages

--- 3_2.py
+++ 3_2.py
@@ -14,6 +14,12 @@ page_url = "http://www.healthcaremagic.com/doctorchatlogs"
 file = open('doctorchatLogs.csv','wb')
 write = csv.writer(file,delimiter=",")
 
+def to_string(variable):
+	variable = variable.encode('ascii','ignore')
+	variable = variable.lstrip()
+	variable = variable.rstrip()
+	return variable
+
 #this function retrives the next page url and the data from that page
 def get_next_page(page_link):
 	page = urllib2.urlopen(page_link)
@@ -28,10 +34,10 @@ def get_next_page(page_link):
 	page_li = pagination.find_all("a",class_="box")
 	for li in page_li:
 		page_li_value = li.string
+		page_li_value = to_string(page_li_value)
 		page_li_url = li.get('href')
-		page_li_value = page_li_value.lstrip()
-		page_li_value = page_li_value.rstrip()
-		match = re.search(r'\D+',page_li_value)
+		page_li_url = to_string(page_li_url)
+		match = re.search(r'Next',page_li_value)
 		if match:
 			match = match.group()
 			if match:
@@ -46,20 +52,15 @@ def get_values(content):
 	for div in all_div:
 		inner_div = div.find("div",class_="smallChatIcon")
 		title = inner_div.a.string
-		title = title.encode('ascii','ignore')		
-		title = title.lstrip()
-		title = title.rstrip()
+		title = to_string(title)
 
 		url = "http://www.healthcaremagic.com" + inner_div.a.get('href')
-		url = url.encode('ascii','ignore')
-		url = url.lstrip()
-		url = url.rstrip()
+		url = to_string(url)
 		
 		match = re.search(r'/([\d]+)',url)
 		match = match.group(1)
 		number = match
-		number = number.lstrip()
-		number = number.rstrip()		
+		number = to_string(number)
 		number = int(number)
 
 		d = [title,url,number]
