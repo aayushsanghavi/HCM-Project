a7f39f0025bbb0f2b82152bbffdefa558b88a2e1
Aayush Sanghavi <aayushsanghavi26@gmail.com>
2018-12-13 17:50:44 +0530

    Fix: 1. Minor changes in 2_1 2. Updates in 3_1

--- 2_1.py
+++ 2_1.py
@@ -1,4 +1,3 @@
-#importing python modules and libraries
 import re
 import sys
 import csv
@@ -11,7 +10,7 @@ logger = logging.getLogger(__name__)
 handler = logging.FileHandler('logfile_2_1.log')
 logger.addHandler(handler)
 
-#this function retrives the title and url of each question of each category
+# retrives the title and url of each question of each category
 def get_values(page_url):
 	page = request.urlopen(page_url).read().decode("utf-8", "ignore")
 	soup = BeautifulSoup(page, "html.parser")
@@ -23,10 +22,10 @@ def get_values(page_url):
 		url = "http://www.healthcaremagic.com" + a.get('href').strip()
 		write.writerow([title, url])
 
-#opens the categories.csv file and retrives previously stored information
+# opens the categories.csv file and retrives previously stored information
 read = csv.reader(open('categories.csv','r'))
 for row in read:
-	#this creates a file for each category and stores all the questions retrived
+	# creates a file for each category and stores all the questions retrived
 	write = csv.writer(open(row[0] + " questions.csv", "w"), delimiter=",")
 	pages = int(int(row[3]) / questions_per_page) + 1
 
@@ -35,7 +34,7 @@ for row in read:
 		try:
 			url = row[1] + "/" + str(page_num)
 			get_values(url)
-		except Exception as e:
+		except:
 			logger.error('Error on page %s', url)
 			logger.error('Failed to get_values', exc_info=True)
 		page_num += 20
--- 3_1.py
+++ 3_1.py
@@ -1,76 +1,39 @@
-#importing python modules and libraries
-import urllib2
 import re
-import csv
-from bs4 import BeautifulSoup
 import sys
-reload(sys)
-sys.setdefaultencoding('utf8')
+import csv
+import logging
+from urllib import request
+from bs4 import BeautifulSoup
 
-#url of the desired webpage
-page_url = "http://www.healthcaremagic.com/premiumquestions"
-
-#this code creates a file premiumQuestions.csv and stores all the information retrived
-file = open('premiumQuestions.csv','wb')
-write = csv.writer(file,delimiter=",")
-
-def to_string(variable):
-	variable = variable.encode('ascii','ignore')
-	variable = variable.lstrip()
-	variable = variable.rstrip()
-	return variable
-
-#this function retrives the next page url and the data from that page
+# retrives the next page url and the data from that page
 def get_next_page(page_link):
-	page = urllib2.urlopen(page_link)
-	soup = BeautifulSoup(page,'lxml')
-	content_div = soup.find("div",class_="mainContent")
-	div = content_div.find("div", style="width:63%; float:left;")
-	page.close()
+	page = request.urlopen(page_url).read().decode("utf-8", "ignore")
+	soup = BeautifulSoup(page, "html.parser")
+	div = soup.find("div", style="width:100%; float:left;")
 	get_values(div)
-	
-	#this segment of code retrives the url of the next page
-	pagination = div.find("div",id="paginationDiv")
-	page_li = pagination.find_all("a",class_="box")
+
+	# retrives the url of the next page
+	page_li = div.find("div", id="paginationDiv").find_all("a", class_="box")
 	for li in page_li:
-		page_li_value = li.string
-		page_li_value = to_string(page_li_value)
-		page_li_url = li.get('href')
-		page_li_url = to_string(page_li_url)
-		match = re.search(r'Next',page_li_value)
-		if match:
-			match = match.group()
-			if match:
-				next_page_url = "http://www.healthcaremagic.com" + page_li_url
-		else:
-			next_page_url = None
-	return next_page_url
-		
-#this function retrives the title, url and number of each category for the specific url
+		page_li_value = li.string.strip()
+		page_li_url = li.get('href').strip()
+		match = re.search(r'Next', page_li_value)
+		if match: return "http://www.healthcaremagic.com" + page_li_url
+	return None
+
+# retrives the title, url and number of each category for the specific url
 def get_values(content):
 	all_div = content.find_all("div",class_="queriesBox")
 	for div in all_div:
 		inner_div = div.find("div",class_="smallPQIcon")
-		title = inner_div.a.string
-		if title:
-			title = to_string(title)
-
-		url = "http://www.healthcaremagic.com" + inner_div.a.get('href')
-		url = to_string(url)
-		
+		title = inner_div.a.string.strip()
+		url = "http://www.healthcaremagic.com" + inner_div.a.get('href').strip()
 		match = re.search(r'/([\d]+)',url)
-		match = match.group(1)
-		number = match
-		number = int(number)
+		number = match.group(1)
+		write.writerow([title, url, number])
 
-		d = [title,url,number]
-		write.writerow(d)
-
-#this loop extracts all the required information from all the pages
-continue_extraction = True
-while continue_extraction:
-	next_page = get_next_page(page_url)
-	if next_page == None:
-		continue_extraction = False
-	else:
-		page_url = next_page
+page_url = "http://www.healthcaremagic.com/premiumquestions"
+write = csv.writer(open('premiumQuestions.csv','w'), delimiter=",")
+# extracts all the required information from all the pages
+while page_url:
+	page_url = get_next_page(page_url)
